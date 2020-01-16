592d63e37d67e60bb28786754eea68d5b711c4f5
zzyong <zouzhiyong@corp.netease.com>
2020-01-16 11:34:59 +0800

    init Scan

---
+++ iScan/iScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iScan
+//
+//  Created by zzyong on 2020/1/15.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
